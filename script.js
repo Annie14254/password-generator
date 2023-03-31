9a64598185d232c8a4a34f95ba6cdec0dff38e05
unknown <annieyashley@gmail.com>
2023-03-31 16:59:01 -0500

    adds content to README and includes image of deployed site

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbers = [0,1,2,3,4,5,6,7,8,9]
-var specialCharacters = [ "!", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":"];
+var specialCharacters = [ "!", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
 var numberofCharacters
 var validCharacters = []
 var includeSpecialCharacters
@@ -62,6 +62,7 @@ function returnError () {
   }
 }
 
+// runs all previous functions and prints the final password
 function generatePassword() {
   passwordCriteria();
   addValidCharacters();
